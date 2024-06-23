954a721e9a0efcbd310f382b89439910058a3e37
benmecha <dali@keytchens.com>
2024-06-23 15:19:46 +0200

    Create Task Entity

---
+++ src/Entity/Task.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Column;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[ORM\Index(name: 'FK__TASK__USER_ID', columns: ['user_id'])]
+class Task
+{
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: false)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: false)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::STRING, enumType: Status::class, nullable: false)]
+    private Status $status;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'tasks')]
+    private User $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStatus(): ?Status
+    {
+        return $this->status;
+    }
+
+    public function setStatus(Status $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+}
